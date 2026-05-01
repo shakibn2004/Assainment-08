b94bde6b65b758f67ea0fc66b9ae485e41cacfcf
shakibn2004 <shakibn2004@gmail.com>
2026-05-01 11:52:44 +0600

    json file changed by replacing image links

--- src/Components/PopularCourses.jsx
+++ src/Components/PopularCourses.jsx
@@ -1,4 +1,5 @@
 import { Syne } from "next/font/google";
+import Image from "next/image";
 import Link from "next/link";
 import { FaStarHalfAlt } from "react-icons/fa";
 import { FaArrowRightLong, FaStar } from "react-icons/fa6";
@@ -33,15 +34,15 @@ const PopularCourses = async () => {
             <div className="cards grid grid-cols-4 gap-4">
                 {
                     coursesSlice.map(course => {
-                        const {id, rating, duration, title, instructor, category, level} = course;
+                        const {id, rating, duration, title, instructor, category, level, image} = course;
 
                         return (
                             <div key={id} className="rounded-2xl shadow-[0_0_2px_#fff]/40">
-                                <div className="bg-amber-300/30 h-40 rounded-t-2xl">
+                                <Image src={image} width={0} height={0} style={{width: '100%', height: '100%'}} alt={id} className="bg-amber-300/30 h-40 rounded-t-2xl">
                                     <div className="cbadge p-4">
                                         <h1 className="text-[#4ade80] text-[14px] rounded-[4px] border border-[#4ade80]/40 px-3 py-1 bg-[#4ade80]/20 w-fit uppercase">{level}</h1>
                                     </div>
-                                </div>
+                                </Image>
 
                                 <div className="content mt-4 space-y-3 px-4">
                                     <h3 className="text-[#f97316]">{category}</h3>
@@ -50,7 +51,7 @@ const PopularCourses = async () => {
                                     <div className="ratting flex justify-between">
                                         <h5 className="flex items-center text-[#fbbf24] text-[14px]">
                                             {
-                                                [<FaStar key={id} />, <FaStar key={id} />, <FaStar key={id} />, <FaStar key={id} />, <FaStarHalfAlt key={id} />].map(icon => icon)
+                                                [<FaStar key={id} />, <FaStar key={id} />, <FaStar key={id} />, <FaStar key={id} />, <FaStarHalfAlt key={title} />].map(icon => icon)
                                             }
                                             <span className="ml-1 font-bold text-white text-[18px]">{rating}</span>
                                         </h5>
