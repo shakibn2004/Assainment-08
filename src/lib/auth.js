e9bfcbba91df36aed098f4f227879682c8e5e83e
shakibn2004 <shakibn2004@gmail.com>
2026-05-01 23:53:49 +0600

    Deployment Problems Solved

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,7 +2,7 @@ import { betterAuth } from "better-auth";
 import { MongoClient } from "mongodb";
 import { mongodbAdapter } from "better-auth/adapters/mongodb";
 
-const client = new MongoClient(process.env.MONGO_URI);
+const client = new MongoClient("mongodb+srv://shakibn2004:N.s602643@cluster0.ij4qzmh.mongodb.net/?appName=Cluster0");
 const db = client.db('shakib');
 
 export const auth = betterAuth({
