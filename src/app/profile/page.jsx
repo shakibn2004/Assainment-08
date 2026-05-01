0964f847f394631c169d8c3ee8df78f0b19fc5b7
shakibn2004 <shakibn2004@gmail.com>
2026-05-02 01:41:41 +0600

    Deploy test

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,20 +1,27 @@
-'use client'
-import Navbar from '@/Components/Navbar';
-import { authClient } from '@/lib/auth-client';
-import Link from 'next/link';
+import { Syne } from 'next/font/google';
+import React from 'react';
+import { IoIosStar } from 'react-icons/io';
+
+
+const syne = Syne({
+    subsets: ['latin'],
+    display: 'swap',
+})
 
 const Profile = () => {
-    const { data: session } = authClient.useSession();
-    const user = session?.user;
-
     return (
-        <div className='text-white'>
-            {
-                user ? <h1>Wellcome Mr.{user.name}</h1> : <Link href={'/auth/signin'}>Login</Link>
-            }
+        <section className='text-white w-[90%] max-w-360 mx-auto py-20 space-y-3'>
+            <div className="top">
+                <h1 className={`${syne.className} text-5xl font-black`}>My Profile</h1>
+            </div>
 
-        </div>
+            <div className="middle">
+                View and manage your learning progress and account details.
+            </div>
+
+        </section>
     );
 };
 
 export default Profile;
+// user ? <h1>Wellcome Mr.{user.name}</h1> : <Link href={'/auth/signin'}>Login</Link>
